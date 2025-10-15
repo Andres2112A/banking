0d79c30ef5ee768d88bd149de7737a2b4b171992
Andres2112A <santiagoacostab.12@gmail.com>
2025-10-15 16:26:04 -0500

    correccion taller

--- authentication/models.py
+++ authentication/models.py
@@ -1,20 +1,12 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
+from django import forms
 from .models import Country
-from .forms import CountryForm
 
-# Create your models here.
-def country_list(request):
-    countries = Country.objects.all().order_by('name')
-    return render(request, 'banking_app/country_list.html', {'countries': countries})
-def country_edit(request, pk):
-    country = get_object_or_404(Country, pk=pk)
-    if request.method == 'POST':
-        form = CountryForm(request.POST, instance=country)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'País actualizado correctamente.')
-            return redirect('country_list')
-    else:
-        form = CountryForm(instance=country)
-    return render(request, 'banking_app/country_form.html', {'form': form, 'country': country})
+class CountryForm(forms.ModelForm):
+	class Meta:
+		model = Country
+		fields = ['name', 'abbrev', 'status']
+		widgets = {
+			'name': forms.TextInput(attrs={'class': 'form-control'}),
+			'abbrev': forms.TextInput(attrs={'class': 'form-control'}),
+			'status': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+		}
--- authentication/urls.py
+++ authentication/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
 path('countries/', views.country_list, name='country_list'),
 path('countries/<int:pk>/edit/', views.country_edit, name='country_edit'),
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('', include('authentication_app.urls')),
+]
